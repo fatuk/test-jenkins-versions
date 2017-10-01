cbcba741e361d885b6578790a61aa13e145a7fe2
Andrew Fatuk <fatukk@gmail.com>
2017-10-01 21:23:54 +0300

    Add update download page js

---
+++ update-download-page.js
@@ -0,0 +1,48 @@
+var shell = require('shelljs');
+var fs = require('fs');
+var Handlebars = require('handlebars');
+var compiledTemplate;
+var downloadPage;
+var links = [];
+var DOWNLOAD_URL = 'http://download.dev.hyperionix.com/';
+
+if (!shell.which('aws')) {
+    shell.echo('Sorry, this script requires aws');
+    shell.exit(1);
+}
+
+shell.mkdir('build');
+shell.mkdir('build/index');
+
+fs.readFile('download.handlebars', 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    compiledTemplate = Handlebars.compile(data);
+});
+
+shell.exec('aws s3 ls s3://download.dev.hyperionix.com', function (status, res) {
+    var folders = res.split('\n');
+    folders.forEach(function (folder) {
+        if (folder !== 'index') {
+            links.push({
+                link: DOWNLOAD_URL + folder,
+                text: folder
+            });
+        }
+    });
+    downloadPage = compiledTemplate({
+        downloadLinks: links
+    });
+});
+
+setTimeout(function () {
+    fs.writeFile('build/index/download.html', downloadPage, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    
+        console.log('The file was saved!');
+        shell.exec('aws s3 sync ./build s3://download.dev.hyperionix.com --acl public-read')
+    });
+}, 5000);
