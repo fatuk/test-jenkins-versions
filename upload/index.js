76ff4b6271e80e0d965c1936ac95737c8f0ddd4f
fatuk <fatukk@gmail.com>
2017-10-03 14:15:39 +0300

    Fix uploader

--- upload/index.js
+++ upload/index.js
@@ -1,4 +1,5 @@
 var shell = require('shelljs');
+var path = require('path');
 var fs = require('fs');
 var Handlebars = require('handlebars');
 var compiledTemplate;
@@ -6,7 +7,7 @@ var downloadPage;
 var links = [];
 var DOWNLOAD_URL = 'http://download.dev.hyperionix.com/';
 var DIST = '../../bin/win32/exe/ReleaseWIN32_x64/setup.exe';
-var version = shell.env['version'];
+var version = (shell.env['version'] && path.basename(shell.env['version'])) || 'none';
 var UPLOAD_DIR = 'toUpload';
 
 if (!shell.which('aws')) {
@@ -14,6 +15,9 @@ if (!shell.which('aws')) {
     shell.exit(1);
 }
 
+console.log('version: ', version);
+
+shell.rm('-rf', UPLOAD_DIR);
 shell.mkdir(UPLOAD_DIR);
 shell.mkdir(UPLOAD_DIR + '/index');
 shell.mkdir(UPLOAD_DIR + '/' + version);
@@ -37,6 +41,11 @@ fs.readFile('download.handlebars', 'utf8', function (err, data) {
                     text: folderNormalized
                 });
             }
+        });
+        // Add a new version
+        links.push({
+            link: DOWNLOAD_URL + version + '/setup.exe',
+            text: version
         });
         downloadPage = compiledTemplate({
             downloadLinks: links
