8c9915cfe6ffcc0344a139add9d6ad5cc8658a95
fatuk <fatukk@gmail.com>
2017-10-03 12:04:58 +0300

    Fix uploader

--- upload/index.js
+++ upload/index.js
@@ -6,7 +6,7 @@ var downloadPage;
 var links = [];
 var DOWNLOAD_URL = 'http://download.dev.hyperionix.com/';
 var DIST = '../../bin/win32/exe/ReleaseWIN32_x64/setup.exe';
-var version = env['version'];
+var version = shell.env['version'];
 var UPLOAD_DIR = 'toUpload';
 
 if (!shell.which('aws')) {
@@ -25,32 +25,30 @@ fs.readFile('download.handlebars', 'utf8', function (err, data) {
         return console.log(err);
     }
     compiledTemplate = Handlebars.compile(data);
-});
 
-// Get s3 bucket folders list
-shell.exec('aws s3 ls s3://download.dev.hyperionix.com', function (status, res) {
-    var folders = res.split('\n');
-    folders.forEach(function (folder) {
-        var folderNormalized = folder.trim().split(' ')[1] && folder.trim().split(' ')[1].split('/')[0];
-        if (folderNormalized && folderNormalized !== 'index') {
-            links.push({
-                link: DOWNLOAD_URL + folderNormalized + '/setup.exe',
-                text: folderNormalized
-            });
-        }
-    });
-    downloadPage = compiledTemplate({
-        downloadLinks: links
-    });
-});
+    // Get s3 bucket folders list
+    shell.exec('aws s3 ls s3://download.dev.hyperionix.com', function (status, res) {
+        var folders = res.split('\n');
+        folders.forEach(function (folder) {
+            var folderNormalized = folder.trim().split(' ')[1] && folder.trim().split(' ')[1].split('/')[0];
+            if (folderNormalized && folderNormalized !== 'index') {
+                links.push({
+                    link: DOWNLOAD_URL + folderNormalized + '/setup.exe',
+                    text: folderNormalized
+                });
+            }
+        });
+        downloadPage = compiledTemplate({
+            downloadLinks: links
+        });
 
-setTimeout(function () {
-    fs.writeFile(UPLOAD_DIR + '/index/download.html', downloadPage, function (err) {
-        if (err) {
-            return console.log(err);
-        }
+        fs.writeFile(UPLOAD_DIR + '/index/download.html', downloadPage, function (err) {
+            if (err) {
+                return console.log(err);
+            }
     
-        console.log('The file was saved!');
-        shell.exec('aws s3 sync ' + UPLOAD_DIR + ' s3://download.dev.hyperionix.com --acl public-read')
+            console.log('The file was saved!');
+            shell.exec('aws s3 sync ' + UPLOAD_DIR + ' s3://download.dev.hyperionix.com --acl public-read')
+        });
     });
-}, 5000);
+});
